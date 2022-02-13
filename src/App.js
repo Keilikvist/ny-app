d3548ae67e0f67ec103a5882d0b5333168add848
Keilikvist <91397999+Keilikvist@users.noreply.github.com>
2022-02-13 13:18:07 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kayleigh Rundquist and is{" "}
-        <a href="https://github.com/Keilikvist/ny-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kayleigh Rundquist and is{" "}
+          <a href="https://github.com/Keilikvist/ny-app" target="_blank">
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
